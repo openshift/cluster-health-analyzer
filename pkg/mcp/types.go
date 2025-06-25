b48593cae5668635e3dbad1ef8254ce055c43d04
Tomas Remes <tremes@users.noreply.github.com>
2025-06-25 09:37:24 +0200

    feat: add MCP server container with the Incidents tool (#38)

---
+++ pkg/mcp/types.go
@@ -0,0 +1,26 @@
+package mcp
+
+import "github.com/prometheus/common/model"
+
+type Response struct {
+	Incidents Incidents `json:"incidents"`
+}
+
+type Incidents struct {
+	Total     int        `json:"total"`
+	Incidents []Incident `json:"items"`
+}
+
+type Incident struct {
+	GroupId   string `json:"id"`
+	Severity  string `json:"severity"`
+	StartTime string `json:"start_time"`
+	Status    string `json:"status"`
+	EndTime   string `json:"end_time"`
+
+	// TODO
+	URL                string              `json:"-"`
+	Alerts             []model.Metric      `json:"alerts"`
+	AffectedComponents []string            `json:"affected_components"`
+	ComponentsSet      map[string]struct{} `json:"-"`
+}
